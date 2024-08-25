b953305a0607d82006c13cdd7882bf6631d5a93f
coder-samurai <aelboutaheri@gmail.com>
2024-08-25 22:37:59 +0200

    fixing the style

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function AddTask() {
+   if (document.getElementById("user-input").value == "") {
+      alert("Please enter a task!");
+      return;
+   }
    var content = document.getElementById("user-input").value;
    var element = document.getElementById("task-container");
 
    var newElement = document.createElement('div');
    newElement.classList.add('ind-task');
-   newElement.innerHTML = '<h2>' + content + '</h2><button onclick="DeleteTask(event)">Delete</button>';
+   const deleteButtonIcon = '<i class="fa-solid fa-trash"></i>'
+   newElement.innerHTML = '<div class="task-content-container"><h2 class="task-content">' + content + '</h2><button class="delete-button" onclick="DeleteTask(event)">delete</button></div>';
 
    element.appendChild(newElement);
 }
